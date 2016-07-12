7409e1674f924e703ec4a5334f1fd6d0251a810a
TSoumanoASC <mokos555@yahoo.com>
2016-07-12 15:04:14 -0400

    Robo Codes added

--- Robo Dance.py
+++ Robo Dance.py
@@ -12,3 +12,35 @@ while t > 2:
     turnRight(.1, .3)
     turnLeft(.1, .3)
     t = t + 1
+    
+    
+    
+    
+    
+from Myro import *
+#for sliding
+for seconds in timer(2):
+    motors(0,1)
+    stop()
+for seconds in timer(2):
+    motors(0,-1)
+    stop()
+for seconds in timer(2):
+    motors(1,0)
+    stop()
+for seconds in timer(2):
+    motors(-1,0)
+    stop()
+## ##for wiggle
+## for seconds in timer(2):
+##     motors(0,1)
+##     stop()
+## for seconds in timer(2):
+##     motors(0,-1)
+##     stop()
+## for seconds in timer(2):
+##     motors(1,0)
+##     stop()
+## for seconds in timer(2):
+##     motors(-1,0)
+##     stop()
