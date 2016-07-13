98161cd92c0537f439c7e04653cceedd568e9527
TSoumanoASC <mokos555@yahoo.com>
2016-07-13 09:31:41 -0400

    added Text

--- Robo Dance.py
+++ Robo Dance.py
@@ -17,30 +17,3 @@ while t > 2:
     
     
     
-from Myro import *
-#for sliding
-for seconds in timer(2):
-    motors(0,1)
-    stop()
-for seconds in timer(2):
-    motors(0,-1)
-    stop()
-for seconds in timer(2):
-    motors(1,0)
-    stop()
-for seconds in timer(2):
-    motors(-1,0)
-    stop()
-## ##for wiggle
-## for seconds in timer(2):
-##     motors(0,1)
-##     stop()
-## for seconds in timer(2):
-##     motors(0,-1)
-##     stop()
-## for seconds in timer(2):
-##     motors(1,0)
-##     stop()
-## for seconds in timer(2):
-##     motors(-1,0)
-##     stop()
---
+++ Test.py
@@ -0,0 +1,12 @@
+from Myro import *
+init("sim")
+
+
+penDown()
+forward (1, 5)
+turnBy(90, "deg")
+backward(2, 2)
+turnBy(90, "deg")
+forward(1, 5)
+turnBy( 270, "deg")
+forward(1, 4.2)
