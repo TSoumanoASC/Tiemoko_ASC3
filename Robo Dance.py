45bc3557f61101bc6058fad6697a94e2f13da9a3
TSoumanoASC <mokos555@yahoo.com>
2016-07-13 10:19:30 -0400

    New Robo Dance

--- Robo Dance.py
+++ Robo Dance.py
@@ -1,11 +1,64 @@
+init("sim")
+    
+for seconds in timer(2):
+    turnBy(270, "deg")
+    turnBy(90, "deg")
+    
+    
+   ## i =0
+
+## while i < 2:      # Robot "Clapping"
+##     beep(.1233333, 900)
+##     wait(0.7)
+##     beep(.1233333, 900)
+##     wait(0.3)
+##     beep(.1233333, 900)
+##     wait(0.4)
+##     beep(.5, 900)
+##     wait(1.6)
+##     i = i + 1
+for seconds in timer(1):   #To the left
+    rotate(10)
+    stop()
+wait(1)
+backward(3,.3)
+wait(1)
+forward(5,.3)           #one hop this time
+wait(1)
+motors(0,1)
+wait(1)
+motors (1,0)
+wait(1)
+                     #cha chat real smooth
+t = 0
+while t < 2:
+    backward(10, 1.99)
+    turnRight(.1, .3)
+    turnLeft(.1, .3)
+    t = t + 1
+
+
+
+
+
+
 from Myro import *
-init ("COM*")
+init ("sim")
+x = 0
+for x in range(0, 4):
+    beep(.233333,900)
+
+
+
+
+
+
 i = 0
 while i < 6:
-beep(.1233333, 900)
-i = i + 1
+    beep(.233333, 900)
+    i = i + 1
 for seconds in timer(2): motors(0,1) 
-bacstop()
+stop()
 t = 0
 while t > 2:
     backward(10, 1.99)
